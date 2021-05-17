723e688d6bce7955acfb8461fe1760e77ccd5b10
AnhDuc <59306889+anhnganh@users.noreply.github.com>
2021-05-17 10:57:02 +0700

    first commitDEm

---
+++ MVVM/Model/SinhVien.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+   public class SinhVien
+    {
+       
+            private int id;
+            private string ten;
+
+            public int Id { get => id; set => id = value; }
+            public string Ten { get => ten; set => ten = value; }
+
+        public static implicit operator string(SinhVien v)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MVVM/ViewModel/SinhVienViewModel.cs
@@ -0,0 +1,69 @@
+﻿using Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace ViewModel
+{
+  public  class SinhVienViewModel
+    {
+        private ObservableCollection<SinhVien> listSinhVien;
+        public ObservableCollection<SinhVien> ListSinhVien { get => listSinhVien; set => listSinhVien = value; }
+        public SinhVienViewModel()
+        {
+            listSinhVien = new ObservableCollection<SinhVien>();
+            ListSinhVien.Add(new SinhVien() { Id = 1, Ten = "Anh" });
+            ListSinhVien.Add(new SinhVien() { Id = 2, Ten = "Binh" });
+            ListSinhVien.Add(new SinhVien() { Id = 3, Ten = "Cuong" });
+            ListSinhVien.Add(new SinhVien() { Id = 4, Ten = "Duong" });
+
+            DeleteCommand = new RelayCommand<SinhVien>(
+                (p) => p != null, // CanExecute()
+             (p) => { 
+                 ListSinhVien.Remove(p as SinhVien); }
+             // Execute()
+                );
+            AddCommand = new RelayCommand<UIElementCollection>(
+           (s) => true, // CanExecute()
+           (s) => {
+               int id = 0;
+               string ten = "";
+               bool isIDint = false;
+               foreach (var item in s)
+               {
+                   TextBox textBox = item as TextBox;
+                   if (textBox == null)
+                   
+                       continue;
+                   
+                   switch (textBox.Name)
+                   {
+                       case "txbId":
+                           isIDint = Int32.TryParse(textBox.Text, out id);
+                           break;
+                       case "txbTen":
+                           ten = textBox.Text;
+                           break;
+                   }
+
+               }
+               if(!isIDint || string.IsNullOrEmpty(ten))
+               {
+                   return;
+               }
+               SinhVien a = new SinhVien() {Id= id, Ten =ten };
+               ListSinhVien.Add(a);
+           } // Execute()
+           );
+        }
+
+        public ICommand DeleteCommand { get; set; }
+        public ICommand AddCommand { get; set; }
+
+    }
+   
+}
+
